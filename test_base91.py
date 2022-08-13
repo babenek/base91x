1adb139ed6b789c37efdc63ef068e3a69e6c45a3
Roman Babenko <babenek@gmail.com>
2022-08-13 16:25:37 +0300

    Tests robustness

--- test_base91.py
+++ test_base91.py
@@ -1,32 +1,50 @@
-import unittest
+import random
 from random import randbytes
 
 import base91
 
+TEXT = "The quick brown\r\nfox\tjumps\nover\rthe lazy\n\rdog!"
+PANGRAM = "Thequickbrownfoxjumpsoverthelazydog!"
+DATA = [88, 136, 162, 112, 31, 156, 195, 75, 208, 5, 61, 106, 20, 163, 227, 172, 240, 150, 163, 100, 63, 170, 82,
+        175, 58, 17, 203, 5, 3]
 
-class Base91Test(unittest.TestCase):
-    TEXT = "The quick brown\r\nfox\tjumps\nover\rthe lazy\n\rdog!"
-    PANGRAM = "Thequickbrownfoxjumpsoverthelazydog!"
-    DATA = [88, 136, 162, 112, 31, 156, 195, 75, 208, 5, 61, 106, 20, 163, 227, 172, 240, 150, 163, 100, 63, 170, 82,
-            175, 58, 17, 203, 5, 3]
 
-    def test_static_decode(self):
-        data = base91.decode(self.TEXT)
-        self.assertEqual(self.DATA, list(data))
+def test_static_alphabet():
+    assert len(base91.alphabet) == base91.base
 
-    def test_static_encode(self):
-        text = base91.encode(bytes(self.DATA))
-        self.assertEqual(self.PANGRAM, text)
 
-    def test_refurbish_small(self):
-        for n in range(33):
-            original_data = randbytes(n)
-            text = base91.encode(original_data)
-            refurbish_data = base91.decode(text)
-            self.assertEqual(original_data, refurbish_data, text)
+def test_static_decode():
+    data = base91.decode(TEXT)
+    assert list(data) == DATA
 
-    def test_refurbish_large(self):
-        original_data = randbytes(65536)
+
+def test_static_encode():
+    text = base91.encode(bytes(DATA))
+    assert text == PANGRAM
+
+
+def test_refurbish_small():
+    for n in range(33):
+        original_data = randbytes(n)
         text = base91.encode(original_data)
         refurbish_data = base91.decode(text)
-        self.assertEqual(original_data, refurbish_data, text)
+        assert len(original_data) == len(refurbish_data)
+        assert original_data == refurbish_data, text
+
+
+def test_refurbish_large():
+    original_data = randbytes(65536)
+    text = base91.encode(original_data)
+    refurbish_data = base91.decode(text)
+    assert len(original_data) == len(refurbish_data)
+    assert original_data == refurbish_data, text
+
+
+def test_stress_decode():
+    text = ""
+    text_size = random.randint(0, 65536)
+    while text_size > len(text):
+        text += chr(random.randint(0, 0x10FFFF))
+    assert len(text) == text_size
+    data = base91.decode(text)
+    assert len(data) <= text_size
