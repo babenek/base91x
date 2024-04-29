c736ab40b10a767f4a2a0aa9825fb39c1774da97
Roman Babenko <babenek@gmail.com>
2024-04-29 11:47:19 +0300

    hatchling build

--- setup.py
+++
@@ -1,3 +0,0 @@
-import setuptools
-
-setuptools.setup(entry_points={"console_scripts": ["base91x=base91x.__main__:main"]})
--- src/base91x/__init__.py
+++ src/base91x/__init__.py
@@ -2,3 +2,7 @@ from .base91x import BASE91X_ALPHABET
 from .base91x import BASE91X_LEN
 from .base91x import decode
 from .base91x import encode
+
+__all__ = ["__version__"]
+
+__version__ = "1.0.1"
